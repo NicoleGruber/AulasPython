02da79b16b5b963ce289287ffe0ab92cfe9f3162
NicoleGruber <Nica.gruber@gmail.com>
2020-02-03 11:49:33 -0300

    Aula 51

---
+++ Aula51/Livros.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_restful import Api
+
+from Controller.livros_controller import LivroController
+
+app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(LivroController, '/api/livro', endpoint = 'livros')
+api.add_resource(LivroController, '/api/livro/<int:id>', endpoint = 'livro')
+
+app.run(debug=True)
