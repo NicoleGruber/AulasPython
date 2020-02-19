689c6a5d01110da65a77ddad40bb70ea6fa30c82
NicoleGruber <57449553+NicoleGruber@users.noreply.github.com>
2020-02-19 16:09:14 -0300

    Aula 1

---
+++ Aula1/Aula1-Print2.py
@@ -0,0 +1,31 @@
+#--- Aula 2  18-11-2019
+#--- Python e aplicação console - Função Print
+
+# print('''Este final de semana eu fui para a praia''')
+
+nome_completo = '\t Nicole' ' ' 'Gruber \n '
+
+
+# print((nome_completo).count('a'))
+# print((nome_completo).lower())
+# print((nome_completo).upper())
+# print((nome_completo).split(' ') )
+# print((nome_completo).strip().split('a') )
+# print((nome_completo).strip() )
+# print((nome_completo).strip() )
+# print((nome_completo).capitalize() )
+
+pessoa = [" ","Livro", "Azul", "Verde"]
+# print(pessoa)
+# print((' nem ').join(pessoa))
+frase = 'Python'
+# print('a'.join(frase))
+
+print(pessoa[1:])
+print(pessoa[1:3])
+print(pessoa[:2])
+print(frase[5:])
+print(frase[5:10])
+print(frase[:4])
+print(pessoa.count(" Livro ".strip().capitalize()))
+
