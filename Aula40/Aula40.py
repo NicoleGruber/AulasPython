c453e5aa0c428a00ca80a64bb19411627baa47e0
NicoleGruber <57449553+NicoleGruber@users.noreply.github.com>
2020-02-24 21:57:18 -0300

    Aula 40 - Api

---
+++ Aula40/Aula40.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_restful import Api
+from Aula40.Controllers.cerveja_controller import CervejaController
+app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(CervejaController, '/api/cerveja')
+
+@app.route('/')
+def inicio():
+    return 'Bem vindo a API'
+
+app.run(debug=True, port=80)
