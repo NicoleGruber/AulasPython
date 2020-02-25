b454e222576f1cd1d8dd347ed265ffcce85689da
NicoleGruber <57449553+NicoleGruber@users.noreply.github.com>
2020-02-24 22:00:17 -0300

    Aula 50 - Api

--- Aula50/dao/pessoa_dao.py
+++ Aula50/dao/pessoa_dao.py
@@ -6,6 +6,6 @@ class PessoaDao():
     def inserir(self,pessoa):
         return "Inserindo pessoa"
     def alterar(self,id):
-        return "Alterando pessoa de id: {}".format(id)
+        return "Alterando a pessoa de id: {}".format(id)
     def remover(self,id):
         return "Removendo a pessoa de id: {}".format(id)
